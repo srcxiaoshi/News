d9aa8f9c0167af69cf6e2cdd637cc7594dd78366
srcxiaoshi <869259080@qq.com>
2018-09-09 10:44:17 +0800

    空项目

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by 史瑞昌 on 2018/9/9.
+//  Copyright © 2018年 史瑞昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by 史瑞昌 on 2018/9/9.
+//  Copyright © 2018年 史瑞昌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
