d0153b4ee974fe9b5c234f8067f9055540ca08b3
srcxiaoshi <869259080@qq.com>
2018-09-15 11:46:39 +0800

    # 因为xcode 提交缺陷，使用github 提交没有提交上的文件
    # 因为xcode 提交缺陷，使用github 提交没有提交上的文件

---
+++ News/UI/StreamViewController/StreamViewController.h
@@ -0,0 +1,13 @@
+//
+//  StreamViewController.h
+//  News
+//
+//  Created by 史瑞昌 on 2018/9/13.
+//  Copyright © 2018年 史瑞昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StreamViewController : UIViewController
+
+@end
